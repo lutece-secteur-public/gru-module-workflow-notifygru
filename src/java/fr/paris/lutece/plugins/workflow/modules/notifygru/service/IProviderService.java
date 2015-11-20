92800b08e3a48686c7adf2fc898c45f2771e3e59
basseck <bass.seck@atos.net>
2015-11-20 16:26:53 +0100

    WFNOTIFGRU-15 : Adding provider class

---
+++ src/java/fr/paris/lutece/plugins/workflow/modules/notifygru/service/IProviderService.java
@@ -0,0 +1,40 @@
+package fr.paris.lutece.plugins.workflow.modules.notifygru.service;
+
+import java.util.List;
+import java.util.Map;
+
+import fr.paris.lutece.portal.service.security.LuteceUser;
+
+public interface IProviderService {
+	
+	
+	String getUserName(LuteceUser user);
+	
+	String getUserEmail (LuteceUser user);
+	
+	String getUserGuid (LuteceUser user);
+	
+	String getTitle(int idResource);
+	
+	String getDescription(int idResource);
+	
+	List<String> getIdDemand(int idResource);
+	
+	List<String> getIdDemandType(int idResource);
+	
+	String getStatus(int idResource);
+	
+	String getIdStatusCrm(int idResource);
+	
+	String getSenderEmail (int idResource);
+	
+	String getRecipient (int idResource);
+	
+	String getPhoneNumber (int idResource);
+	
+	Object getInfosHelp();
+	
+	String getInfos(int idResource);
+	
+
+}
