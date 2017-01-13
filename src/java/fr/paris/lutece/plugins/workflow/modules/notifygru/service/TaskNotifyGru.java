5e74e194cfe57b30ed57ac4d83c83423144c4680
pplouy <paul.plouy@atos.net>
2017-01-13 11:27:17 +0100

    WFNOTIFGRU-85 : rename DashboardNotification into MyDashboardNotification.java

--- src/java/fr/paris/lutece/plugins/workflow/modules/notifygru/service/TaskNotifyGru.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/notifygru/service/TaskNotifyGru.java
@@ -39,9 +39,9 @@ import fr.paris.lutece.plugins.grubusiness.business.notification.BackofficeNotif
 import fr.paris.lutece.plugins.grubusiness.business.notification.BroadcastNotification;
 import fr.paris.lutece.plugins.grubusiness.business.notification.EmailAddress;
 import fr.paris.lutece.plugins.grubusiness.business.notification.EmailNotification;
+import fr.paris.lutece.plugins.grubusiness.business.notification.MyDashboardNotification;
 import fr.paris.lutece.plugins.grubusiness.business.notification.Notification;
 import fr.paris.lutece.plugins.grubusiness.business.notification.SMSNotification;
-import fr.paris.lutece.plugins.grubusiness.business.notification.DashboardNotification;
 import fr.paris.lutece.plugins.librarynotifygru.services.NotificationService;
 import fr.paris.lutece.plugins.workflow.business.task.TaskDAO;
 import fr.paris.lutece.plugins.workflow.modules.notifygru.business.NotifyGruHistory;
@@ -152,7 +152,7 @@ public class TaskNotifyGru extends SimpleTask
 
             if ( config.isActiveOngletGuichet(  ) )
             {
-                DashboardNotification userDashBoard = buildUserDashboardNotification( config, nIdResourceHistory,
+                MyDashboardNotification userDashBoard = buildUserDashboardNotification( config, nIdResourceHistory,
                         locale );
                 notificationObject.setUserDashboard( userDashBoard );
                 strMessageGuichet = userDashBoard.getMessage(  );
@@ -244,35 +244,34 @@ public class TaskNotifyGru extends SimpleTask
      * @param locale the locale
      * @return the notify gru global notification
      */
-    private Notification buildGlobalNotification( TaskNotifyGruConfig config, int nIdResourceHistory,
-        Locale locale )
+    private Notification buildGlobalNotification( TaskNotifyGruConfig config, int nIdResourceHistory, Locale locale )
     {
         Notification notification = new Notification(  );
 
-        Demand demand = new Demand();
+        Demand demand = new Demand(  );
         /*Le champs permettra de valorisé le champs demand_status du flux notification V1.
         La valeur est à 0 (veut dire « en cours ») ou à 1 (veut dire « clôturée ». On est dans le cas où le checkbox est coché).
             Ce champs est « destiné » à la vue 360° (colonne statut dans la liste des demandes).*/
         demand.setStatusId( config.getDemandStatus(  ) );
         demand.setReference( _notifyGruService.getDemandReference( nIdResourceHistory ) );
         notification.setNotificationDate( System.currentTimeMillis(  ) );
-        demand.setId( String.valueOf(_notifyGruService.getOptionalDemandId( nIdResourceHistory ) ) );
+        demand.setId( String.valueOf( _notifyGruService.getOptionalDemandId( nIdResourceHistory ) ) );
         demand.setTypeId( String.valueOf( _notifyGruService.getOptionalDemandIdType( nIdResourceHistory ) ) );
         demand.setMaxSteps( config.getDemandMaxStepGuichet(  ) );
         demand.setCurrentStep( config.getDemandUserCurrentStepGuichet(  ) );
-        
-        Customer customer = new Customer( );
+
+        Customer customer = new Customer(  );
         customer.setId( _notifyGruService.getCustomerId( nIdResourceHistory ) );
         customer.setAccountGuid( _notifyGruService.getUserGuid( nIdResourceHistory ) );
         customer.setEmail( _notifyGruService.getUserEmail( nIdResourceHistory ) );
         demand.setCustomer( customer );
         notification.setDemand( demand );
 
-        DashboardNotification dashboardNotification = new DashboardNotification( );
+        MyDashboardNotification dashboardNotification = new MyDashboardNotification(  );
         dashboardNotification.setStatusId( config.getCrmStatusId(  ) );
-                
+
         notification.setUserDashboard( dashboardNotification );
-        
+
         return notification;
     }
 
@@ -284,10 +283,10 @@ public class TaskNotifyGru extends SimpleTask
      * @param locale the locale
      * @return the notify gru userDashboard notification
      */
-    private DashboardNotification buildUserDashboardNotification( TaskNotifyGruConfig config,
-        int nIdResourceHistory, Locale locale )
+    private MyDashboardNotification buildUserDashboardNotification( TaskNotifyGruConfig config, int nIdResourceHistory,
+        Locale locale )
     {
-        DashboardNotification userDashBoard = new DashboardNotification(  );
+        MyDashboardNotification userDashBoard = new MyDashboardNotification(  );
 
         String strMessageUserDashboard = giveMeTexteWithValueOfMarker( config.getMessageGuichet(  ), locale,
                 _notifyGruService.getInfos( nIdResourceHistory ) );
@@ -298,6 +297,7 @@ public class TaskNotifyGru extends SimpleTask
         String strStatustextDashboard = giveMeTexteWithValueOfMarker( config.getStatustextGuichet(  ), locale,
                 _notifyGruService.getInfos( nIdResourceHistory ) );
 
+        userDashBoard.setStatusId( config.getCrmStatusId(  ) );
         userDashBoard.setStatusText( strStatustextDashboard );
         userDashBoard.setSenderName( config.getSenderNameGuichet(  ) );
         userDashBoard.setSubject( strSubjectUserDashboard );
