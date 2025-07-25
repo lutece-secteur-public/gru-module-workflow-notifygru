54b6c11412fbaa645f2ccab78563e81f3af2fe6c
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-07-25 18:05:27 +0200

    LUT-30461 : Adding liquibase tags to SQL scripts (Lutèce 8)

--- src/sql/plugins/workflow/modules/notifygru/upgrade/upgrade_db_workflow_notifygru-2.0.0-2.0.1.sql
+++ src/sql/plugins/workflow/modules/notifygru/upgrade/upgrade_db_workflow_notifygru-2.0.0-2.0.1.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+--changeset workflow-notifygru:upgrade_db_workflow_notifygru-2.0.0-2.0.1.sql
+--preconditions onFail:MARK_RAN onError:WARN
 
 ALTER TABLE workflow_task_notify_gru_history ADD COLUMN code_event VARCHAR(45) NULL AFTER is_active_onglet_broadcast;
 ALTER TABLE workflow_task_notify_gru_history ADD COLUMN type_event VARCHAR(45) NULL AFTER code_event;
--- src/sql/plugins/workflow/modules/notifygru/upgrade/upgrade_db_workflow_notifygru-2.1.5-2.1.6.sql
+++ src/sql/plugins/workflow/modules/notifygru/upgrade/upgrade_db_workflow_notifygru-2.1.5-2.1.6.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+--changeset workflow-notifygru:upgrade_db_workflow_notifygru-2.1.5-2.1.6.sql
+--preconditions onFail:MARK_RAN onError:WARN
 -- Suppression de la colonne content_cleaned de la table workflow_task_notify_gru_cf
 ALTER TABLE workflow_task_notify_gru_cf DROP COLUMN content_cleaned;
 
