9b4c28008cde5059e2f7fda3a9348968eba0d667
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-07-25 16:52:52 +0200

    LUT-30460 : Adding liquibase tags to SQL scripts (Lutèce 7)

--- src/sql/plugins/workflow/modules/notifygru/upgrade/upgrade_db_workflow_notifygru-1.3.3-1.3.4.sql
+++ src/sql/plugins/workflow/modules/notifygru/upgrade/upgrade_db_workflow_notifygru-1.3.3-1.3.4.sql
@@ -1,1 +1,4 @@
+--liquibase formatted sql
+--changeset workflow-notifygru:upgrade_db_workflow_notifygru-1.3.3-1.3.4.sql
+--preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE workflow_task_notify_gru_history DROP PRIMARY KEY, ADD PRIMARY KEY(id_history, id_task);
