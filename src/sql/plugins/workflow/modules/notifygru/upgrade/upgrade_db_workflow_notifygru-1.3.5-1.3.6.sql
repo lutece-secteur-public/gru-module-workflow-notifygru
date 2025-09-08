5b8c01d9b07129ce63179861fe83ee11e27187ad
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-09-08 14:32:35 +0200

    LUT-30678 : workflow_task_notify_gru_history table creation not working on MYSQL + update liquibase tags for MySQL
    (cherry picked from commit 0fe59a3c3d4c92dd09568c785630995864ee3a21)

--- src/sql/plugins/workflow/modules/notifygru/upgrade/upgrade_db_workflow_notifygru-1.3.5-1.3.6.sql
+++ src/sql/plugins/workflow/modules/notifygru/upgrade/upgrade_db_workflow_notifygru-1.3.5-1.3.6.sql
@@ -1,4 +1,4 @@
---liquibase formatted sql
---changeset workflow-notifygru:upgrade_db_workflow_notifygru-1.3.5-1.3.6.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset workflow-notifygru:upgrade_db_workflow_notifygru-1.3.5-1.3.6.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE workflow_task_notify_gru_cf ADD COLUMN marker_provider_ids VARCHAR(1000) NULL AFTER id_spring_provider;
